982815e153a68a2f77f2232a7e0471a379381c17
M3156 <m3156@student.jamk.fi>
2020-01-31 10:30:29 +0200

    FootballStatsApp

---
+++ FootballStatsApp/FootballStats/Data/Match.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace FootballStats.Data
+{
+    public class Match
+    {
+        public int Id { get; set; }
+        public object Round { get; set; }
+        public int RoundNumber { get; set; }
+        public DateTime MatchDate { get; set; }
+        public Team HomeTeam { get; set; }
+        public Team AwayTeam { get; set; }
+        public int HomeGoals { get; set; }
+        public int AwayGoals { get; set; }
+        public int Status { get; set; }
+        public int PlayedMinutes { get; set; }
+        public object SecondHalfStarted { get; set; }
+        public DateTime? GameStarted { get; set; }
+        public List<MatchEvent> MatchEvents { get; set; }
+        public List<object> PeriodResults { get; set; }
+        public bool OnlyResultAvailable { get; set; }
+        public int Season { get; set; }
+        public string Country { get; set; }
+        public string League { get; set; }
+    }
+    
+
+    public partial class Team
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string FullName { get; set; }
+        public object Logo { get; set; }
+        public string LogoUrl { get; set; }
+        public int Ranking { get; set; }
+        public string Message { get; set; }
+    }
+
+    public partial class MatchEvent
+    {
+        public long Id { get; set; }
+
+        public long MatchId { get; set; }
+
+        public long EventMinute { get; set; }
+
+        public long ElapsedSeconds { get; set; }
+
+        public long TeamId { get; set; }
+
+        public string Description { get; set; }
+
+        public string FullDescription { get; set; }
+
+        public Uri EventTypeIcon { get; set; }
+
+        public string EventType { get; set; }
+
+        public long EventTypeEnum { get; set; }
+
+        public long PlayerId { get; set; }
+
+        public object Player { get; set; }
+
+        public string Identifier { get; set; }
+
+        public object AssistPlayers { get; set; }
+
+        public object AssistPlayerNames { get; set; }
+
+        public object Modifier { get; set; }
+
+        public object Score { get; set; }
+
+        public bool IsGoal { get; set; }
+    }
+}
---
+++ FootballStatsApp/FootballStats/Data/MatchAPI.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Text;
+using System.Runtime.Serialization.Json;
+using System.Net;
+
+namespace FootballStats.Data
+{
+    public class MatchAPI
+    {
+        public List<Match> GetMatches()
+        {
+            string footballJson = "https://functionapp2018071101324.blob.core.windows.net/data/matches_latest.json";
+            using (WebClient wc = new WebClient())
+            {
+                var json = wc.DownloadString(footballJson);
+                var result = JsonSerializer.Deserialize<List<Match>>(json);
+                return result;
+            }
+        }
+
+        public Match GetMatch(int id)
+        {
+            var matches = GetMatches();
+            foreach(Match match in matches)
+            {
+                if(id == match.Id)
+                {
+                    return match;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            return null;
+        }
+    }
+}
