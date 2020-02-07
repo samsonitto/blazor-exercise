4aacf864d4180b35353f111842404703aa29aeda
Samson <samsonitto@gmail.com>
2020-02-07 10:24:03 +0200

    Unit testing alkutoimenpiteet

---
+++ FootballStatsApp/FootballStatsTests/BasicTests.cs
@@ -0,0 +1,20 @@
+using System;
+using Xunit;
+
+namespace FootballStatsTests
+{
+    public class BasicTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+
+        }
+    }
+}
